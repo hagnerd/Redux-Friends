88f64d25ca01f6b6f81ccfb9746171bcd0b32b56
Matt Hagner <web@matthagner.com>
2019-07-18 10:53:30 -0500

    Add a basic dashboard

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import PrivateRoute from "./components/private-route";
 
 import Login from "./pages/login.js";
+import Dashboard from "./pages/dashboard";
 import NoMatch from "./pages/no-match";
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
     <main>
       <Switch>
         <Route path="/login" component={Login} />
+        <PrivateRoute path="/" component={Dashboard} />
         <Route component={NoMatch} />
       </Switch>
     </main>
