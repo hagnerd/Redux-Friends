fbf2bc389b05634e5e009eee2a9b0de449ec5cd0
Matt Hagner <web@matthagner.com>
2019-07-18 08:07:02 -0500

    Make action creator for fetching friends

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -1,10 +1,25 @@
+import axios from "axios";
+
 ///////////////////////////////////////////////////////////////////////////////
 //  ACTIONS
 
-export const FETCH_FRIENDS_START = 'FETCH_FRIENDS_START'
-export const FETCH_FRIENDS_SUCCESS = 'FETCH_FRIENDS_SUCCESS'
-export const FETCH_FRIENDS_FAILURE = 'FETCH_FRIENDS_FAILURE'
+export const FETCH_FRIENDS_START = "FETCH_FRIENDS_START";
+export const FETCH_FRIENDS_SUCCESS = "FETCH_FRIENDS_SUCCESS";
+export const FETCH_FRIENDS_FAILURE = "FETCH_FRIENDS_FAILURE";
 
 ///////////////////////////////////////////////////////////////////////////////
 //  ACTION CREATORS
+export const fetchFriends = () => async dispatch => {
+  dispatch({ type: FETCH_FRIENDS_START });
 
+  return axios
+    .get("http://localhost:5000/api/friends")
+    .then(res => {
+      console.log(res);
+      dispatch({ type: FETCH_FRIENDS_SUCCESS });
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch({ type: FETCH_FRIENDS_FAILURE });
+    });
+};
