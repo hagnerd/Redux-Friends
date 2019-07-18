cdc4041fccc09453b72ba600d7e995824b778b8e
Matt Hagner <web@matthagner.com>
2019-07-18 10:46:21 -0500

    Refactor Login page

--- friends/src/pages/login.js
+++ friends/src/pages/login.js
@@ -1,81 +1,15 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { login } from "../actions";
 
-class Login extends React.Component {
-  state = {
-    username: "",
-    password: ""
-  };
+import LoginForm from "../components/login-form";
 
-  static propTypes = {
-    login: PropTypes.func.isRequired,
-    isLoggingIn: PropTypes.bool.isRequired,
-    errorMessage: PropTypes.string
-  };
-
-  handleChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    const { username, password } = this.state;
-
-    this.props
-      .login(username, password)
-      .then(res => {
-        // TODO: Implement redirecting the user to the `/` route upon successful
-        // login
-        console.log(res);
-      })
-      .catch(err => {
-        console.error(err);
-      });
-  };
-
-  render() {
-    const { username, password } = this.state;
-    const { isLoggingIn, errorMessage } = this.props;
-
-    return (
-      <>
-        {errorMessage ? (
-          <p style={{ color: "red" }}>{errorMessage}</p>
-        ) : (
-          <div style={{ height: "42px" }} />
-        )}
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="username">Username:</label>
-          <input
-            id="username"
-            name="username"
-            type="text"
-            value={username}
-            onChange={this.handleChange}
-          />
-          <br />
-          <label htmlFor="password">Password:</label>
-          <input
-            id="password"
-            name="password"
-            type="password"
-            value={password}
-            onChange={this.handleChange}
-          />
-          <br />
-          {!isLoggingIn ? (
-            <button type="submit">Login</button>
-          ) : (
-            <p>Loading...</p>
-          )}
-        </form>
-      </>
-    );
-  }
+function Login(props) {
+  return (
+    <>
+      <LoginForm {...props} />
+    </>
+  );
 }
 
 export default connect(
