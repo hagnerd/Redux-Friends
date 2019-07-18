0d734a544a1aa97ba29ba7c087c5f187ea57a4fb
Matt Hagner <web@matthagner.com>
2019-07-18 08:35:52 -0500

    Add documentation for axiosAuth

--- friends/src/axiosAuth.js
+++ friends/src/axiosAuth.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+// We can avoid having to rewrite fetching the token from local storage and
+// adding it to our axios requests via the header using the following function
+// in place of normal `axios` calls for api calls taht require the header.
 export default function axiosAuth() {
   const token = localStorage.getItem("token");
 
