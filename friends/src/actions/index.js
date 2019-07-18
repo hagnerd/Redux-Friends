ff25f7e018f17dadf8a7602f4df6e9f9b5cc0785
Matt Hagner <web@matthagner.com>
2019-07-18 08:02:29 -0500

    Create action constants for fetching friends

---
+++ friends/src/actions/index.js
@@ -0,0 +1,10 @@
+///////////////////////////////////////////////////////////////////////////////
+//  ACTIONS
+
+export const FETCH_FRIENDS_START = 'FETCH_FRIENDS_START'
+export const FETCH_FRIENDS_SUCCESS = 'FETCH_FRIENDS_SUCCESS'
+export const FETCH_FRIENDS_FAILURE = 'FETCH_FRIENDS_FAILURE'
+
+///////////////////////////////////////////////////////////////////////////////
+//  ACTION CREATORS
+
