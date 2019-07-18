2377e7670830876c61121318443ab1d0a763fbd9
Matt Hagner <web@matthagner.com>
2019-07-17 21:58:23 -0500

    Set up Redux Devtools

--- friends/src/index.js
+++ friends/src/index.js
@@ -2,13 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 
 import App from "./App";
 import rootStore from "./reducers";
 
-const store = createStore(rootStore, applyMiddleware(thunk, logger));
+const store = createStore(
+  rootStore,
+  compose(
+    applyMiddleware(thunk, logger),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : f => f
+  )
+);
 
 ReactDOM.render(
   <Provider store={store}>
