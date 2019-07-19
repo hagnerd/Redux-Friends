034387e1312f8f7b5f44f8bf5243bcc5201a41c8
Matt Hagner <web@matthagner.com>
2019-07-18 19:05:49 -0500

    Simplify the reducers and actions
    There was a lot of parallel actions and cases in the reducer so I
    decided to pair down. Most of our requests follow a similar pattern:
    we'll send a request to the server, if successful it returns an array of
    friends that we will save, if there is an error we want to take the
    error and display it.
    
    It can be loaded, successful, or error out.
    
    The only unique request is the login, because with a successful login, I
    want to have the addTokenToLocalStorage middleware intercept that token,
    and save it to local storage.

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -4,113 +4,118 @@ import axiosAuth from "../axiosAuth";
 ///////////////////////////////////////////////////////////////////////////////
 //  ACTIONS
 
-export const FETCH_FRIENDS_START = "FETCH_FRIENDS_START";
-export const FETCH_FRIENDS_SUCCESS = "FETCH_FRIENDS_SUCCESS";
-export const FETCH_FRIENDS_FAILURE = "FETCH_FRIENDS_FAILURE";
-
-export const LOGIN_START = "LOGIN_START";
+export const LOADING = "LOADING";
+export const ERROR = "ERROR";
+export const REQUEST_SUCCESS = "REQUEST_SUCCESS";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
-export const LOGIN_FAILURE = "LOGIN_FAILURE";
-
-export const SAVE_FRIENDS_START = "SAVE_FRIENDS_START";
-export const SAVE_FRIENDS_SUCCESS = "SAVE_FRIENDS_SUCCESS";
-export const SAVE_FRIENDS_FAILURE = "SAVE_FRIENDS_FAILURE";
-
-export const UPDATE_FRIEND_START = "UPDATE_FRIEND_START";
-export const UPDATE_FRIEND_SUCCESS = "UPDATE_FRIEND_SUCCESS";
-export const UPDATE_FRIEND_FAILURE = "UPDATE_FRIEND_FAILURE";
-
-export const DELETE_FRIEND_START = "DELETE_FRIEND_START";
-export const DELETE_FRIEND_SUCCESS = "DELETE_FRIEND_SUCCESS";
-export const DELETE_FRIEND_FAILURE = "DELETE_FRIEND_FAILURE";
 
 ///////////////////////////////////////////////////////////////////////////////
 //  ACTION CREATORS
+
+const makeLoggableError = ({ status, statusText, message }) => {
+  return `ERROR - STATUS ${status} ${statusText} - MESSAGE ${message}`;
+};
+
 export const fetchFriends = () => async dispatch => {
-  dispatch({ type: FETCH_FRIENDS_START });
+  dispatch({ type: LOADING });
 
   // We use axiosAuth so that we don't need to manually read the token form
   // localStorage
   return axiosAuth()
     .get("http://localhost:5000/api/friends")
     .then(res => {
-      dispatch({ type: FETCH_FRIENDS_SUCCESS, payload: res.data });
+      dispatch({ type: REQUEST_SUCCESS, payload: res.data });
     })
     .catch(err => {
-      console.log(err);
-      dispatch({ type: FETCH_FRIENDS_FAILURE });
+      dispatch({ type: ERROR, payload: err.response.data.error });
+      return makeLoggableError({
+        status: err.response.status,
+        statusText: err.response.statusText,
+        message: err.response.data.error
+      });
     });
 };
 
 export const login = (username, password) => async dispatch => {
-  dispatch({ type: LOGIN_START });
+  dispatch({ type: LOADING });
+
   return axios
     .post("http://localhost:5000/api/login", { username, password })
     .then(res => {
-      console.log(res);
       dispatch({ type: LOGIN_SUCCESS, payload: { token: res.data.payload } });
     })
     .catch(err => {
       const payload = err.response && err.response.data.error;
-      console.log(err.response);
 
-      dispatch({ type: LOGIN_FAILURE, payload });
-      return `ERROR - STATUS: ${err.response.status} ${
-        err.response.statusText
-      } - MESSAGE: ${payload}`;
+      dispatch({ type: ERROR, payload });
+
+      return makeLoggableError({
+        status: err.response.status,
+        statusText: err.response.statusText,
+        message: payload
+      });
     });
 };
 
 export const saveFriends = body => async dispatch => {
-  dispatch({ type: SAVE_FRIENDS_START });
+  dispatch({ type: LOADING });
 
   return axiosAuth()
     .post("http://localhost:5000/api/friends", body)
     .then(res => {
-      dispatch({ type: SAVE_FRIENDS_SUCCESS, payload: res.data });
+      dispatch({ type: REQUEST_SUCCESS, payload: res.data });
     })
     .catch(err => {
       const payload = err.response && err.response.data.error;
 
-      dispatch({ type: SAVE_FRIENDS_FAILURE, payload });
+      dispatch({ type: ERROR, payload });
 
-      return `ERROR - STATUS: ${err.response.status} ${
-        err.response.statusText
-      } - MESSAGE ${payload}`;
+      return makeLoggableError({
+        status: err.response.status,
+        statusText: err.response.statusText,
+        message: payload
+      });
     });
 };
 
 export const updateFriend = (id, body) => async dispatch => {
-  dispatch({ type: UPDATE_FRIEND_START });
+  dispatch({ type: LOADING });
 
   return axiosAuth()
     .put(`http://localhost:5000/api/friends/${id}`, body)
     .then(res => {
-      dispatch({ type: UPDATE_FRIEND_SUCCESS, payload: res.data });
+      dispatch({ type: REQUEST_SUCCESS, payload: res.data });
     })
     .catch(err => {
-      console.log(err);
-      const payload = err.response && err.response.data;
+      const payload = err.response && err.response.data.error;
 
-      dispatch({ type: UPDATE_FRIEND_FAILURE, payload });
-      return `ERROR - STATUS ${err.response.status} ${
-        err.response.statusText
-      } - MESSAGE ${payload}`;
+      dispatch({ type: ERROR, payload });
+
+      return makeLoggableError({
+        status: err.response.status,
+        statusText: err.response.statusText,
+        message: payload
+      });
     });
 };
 
 export const deleteFriend = id => async dispatch => {
-  dispatch({ type: DELETE_FRIEND_START });
+  dispatch({ type: LOADING });
 
   return axiosAuth()
     .delete(`http://localhost:5000/api/friends/${id}`)
     .then(res => {
-      dispatch({ type: DELETE_FRIEND_SUCCESS, payload: res.data });
+      dispatch({ type: REQUEST_SUCCESS, payload: res.data });
     })
     .catch(err => {
-      dispatch({ type: DELETE_FRIEND_FAILURE, payload: err.response.data });
-      return `ERROR - STATUS ${err.response.status} ${
-        err.response.statusText
-      } - MESSAGE ${err.response.data}`;
+      const payload = err.response && err.response.data.error;
+
+      dispatch({ type: ERROR, payload });
+
+      return makeLoggableError({
+        status: err.response.status,
+        statusText: err.response.statusText,
+        message: payload
+      });
     });
 };
--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -1,138 +1,40 @@
-import {
-  LOGIN_START,
-  LOGIN_SUCCESS,
-  LOGIN_FAILURE,
-  FETCH_FRIENDS_START,
-  FETCH_FRIENDS_SUCCESS,
-  FETCH_FRIENDS_FAILURE,
-  SAVE_FRIENDS_START,
-  SAVE_FRIENDS_SUCCESS,
-  SAVE_FRIENDS_FAILURE,
-  UPDATE_FRIEND_START,
-  UPDATE_FRIEND_SUCCESS,
-  UPDATE_FRIEND_FAILURE,
-  DELETE_FRIEND_START,
-  DELETE_FRIEND_SUCCESS,
-  DELETE_FRIEND_FAILURE
-} from "../actions";
+import { LOADING, ERROR, REQUEST_SUCCESS, LOGIN_SUCCESS } from "../actions";
 
 const initialState = {
   friends: [],
-  loggingIn: false,
-  errorMessage: null,
-  isFetchingFriends: false,
-  isSavingFriends: false,
-  isUpdatingFriend: false,
-  isDeletingFriend: false
+  isLoading: false,
+  errorMessage: null
 };
 
 export default function rootReducer(state = initialState, action) {
   switch (action.type) {
-    case LOGIN_START: {
+    case LOADING: {
       return {
         ...state,
-        loggingIn: true
+        isLoading: true,
+        errorMessage: null
+      };
+    }
+    case REQUEST_SUCCESS: {
+      return {
+        ...state,
+        isLoading: false,
+        errorMessage: null,
+        friends: action.payload
+      };
+    }
+    case ERROR: {
+      return {
+        ...state,
+        isLoading: false,
+        errorMessage: action.payload
       };
     }
     case LOGIN_SUCCESS: {
       return {
         ...state,
-        loggingIn: false,
-        errorMessage: null
-      };
-    }
-    case LOGIN_FAILURE: {
-      return {
-        ...state,
-        loggingIn: false,
-        errorMessage: action.payload
-      };
-    }
-
-    case FETCH_FRIENDS_START: {
-      return {
-        ...state,
-        isFetchingFriends: true
-      };
-    }
-    case FETCH_FRIENDS_SUCCESS: {
-      return {
-        ...state,
-        isFetchingFriends: false,
-        errorMessage: null,
-        friends: action.payload
-      };
-    }
-
-    case FETCH_FRIENDS_FAILURE: {
-      return {
-        ...state,
-        isFetchingFriends: false,
-        errorMessage: "An error occured while fetching your friends"
-      };
-    }
-    case SAVE_FRIENDS_START: {
-      return {
-        ...state,
-        isSavingFriends: true
-      };
-    }
-    case SAVE_FRIENDS_SUCCESS: {
-      return {
-        ...state,
-        isSavingFriends: false,
-        friends: action.payload,
-        errorMessage: null
-      };
-    }
-    case SAVE_FRIENDS_FAILURE: {
-      return {
-        ...state,
-        isSavingFriends: false,
-        errorMessage: action.payload
-      };
-    }
-
-    case UPDATE_FRIEND_START: {
-      return {
-        ...state,
-        isUpdatingFriend: true
-      };
-    }
-    case UPDATE_FRIEND_SUCCESS: {
-      return {
-        ...state,
-        isUpdatingFriend: false,
-        friends: action.payload,
-        errorMessage: null
-      };
-    }
-    case UPDATE_FRIEND_FAILURE: {
-      return {
-        ...state,
-        isUpdatingFriend: false,
-        errorMessage: action.payload
-      };
-    }
-    case DELETE_FRIEND_START: {
-      return {
-        ...state,
-        isDeletingFriend: true
-      };
-    }
-    case DELETE_FRIEND_SUCCESS: {
-      return {
-        ...state,
-        isDeletingFriend: false,
-        errorMessage: null,
-        friends: action.payload
-      };
-    }
-    case DELETE_FRIEND_FAILURE: {
-      return {
-        ...state,
-        isDeletingFriend: false,
-        errorMessage: action.payload
+        isLoading: false,
+        erroMessage: null
       };
     }
     default:
